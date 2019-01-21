73b507af23dbe17006880619663c7cc0b8e8fabb
Michael Wright <mwright@spartaglobal.com>
2019-01-21 10:20:49 +0000

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener(DOMContentLoaded, function() {
+
+
+    var canvas = document.getElementById('canvas');
+    var c = canvas.getContext('2d');
+     
+
+
+
+
+
+}); //DOM Content closer
+
+// --------+== PseudoCode ==+-----------
+
+// ------ GRID -------
+//Design grid - 6x6 grid
+// Set starting positions of different levels
+    // 3 levels for first sprint
+
+// ----- PIECES -------
+// Initiate pieces
+// Dimensions of pieces - 2x1, 3x1
+// Fixed direction of pieces
+// Movement of pieces
+    // Limited to up-to-next movement
+
+// ----- WIN ------
+// If the ferrari reaches a fixed coordinate
+// Add maximum move count
+
+// ------ BUTTON ------
+// Reset Button
+// Undo
+// Level Selector
