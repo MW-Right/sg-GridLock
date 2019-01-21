d8982913a5f91d1a5edc076a266f83474b1a301d
Michael Wright <mwright@spartaglobal.com>
2019-01-21 16:18:25 +0000

    Adding interactivity

--- js/app.js
+++ js/app.js
@@ -1,64 +1,79 @@
-document.addEventListener("DOMContentLoaded", function() {
 
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
+// Defining Canvas
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+var elements = [];
 
-    class Grid {
-        constructor(gridDim, pd) {
-            this.gridDim = gridDim;
-            this.pd = pd; 
-            this.gridWidth = function() {
-
-            }
+class Grid {
+    constructor(gridDim, pd) {
+        this.gridDim = gridDim;
+        this.pd = pd;
+    }
+    gridDrawing() {
+        for (let i = 0; i < (this.gridDim + 1); i++){      // grid dimensions only need one variable as the board is square
+            ctx.moveTo(this.pd, this.pd + (100 * i));
+            ctx.lineTo(this.pd + (100 * this.gridDim), this.pd + (100 * i));   
         }
-        gridDrawing() {
-            for (let i = 0; i < (this.gridDim + 1); i++){      // grid dimensions only need one variable as the board is square
-                ctx.moveTo(this.pd, this.pd + (100 * i));
-                ctx.lineTo(this.pd + (100 * this.gridDim), this.pd + (100 * i));   
-            }
-            for (let i = 0; i < (this.gridDim + 1); i++) {
-                ctx.moveTo(this.pd + (100 * i), this.pd);
-                ctx.lineTo(this.pd + (100 * i), this.pd + (100 * this.gridDim));
-            }
-            ctx.strokeStyle = "#000";
+        for (let i = 0; i < (this.gridDim + 1); i++) {
+            ctx.moveTo(this.pd + (100 * i), this.pd);
+            ctx.lineTo(this.pd + (100 * i), this.pd + (100 * this.gridDim));
+        }
+        ctx.strokeStyle = "#000";
+        ctx.stroke();
+    }
+}
+
+class Piece {
+    constructor(x, y, length, direction) {
+        this.length = length;
+        // Length can only be 2 or 3
+        this.x = x * 100;
+        this.y = y * 100;
+        this.direction = direction;
+        this.active = true
+    }
+    
+    drawPiece() {
+        ctx.fillStyle = "tomato"
+        var l = (((this.length * 100) - 10) / this.length);
+        if (this.direction === "vert") {
+            ctx.fillStyle = "blue";
+            ctx.fillRect(this.x - 85, this.y + 15, 90, (this.length * l));
             ctx.stroke();
-        }
+        } else if (this.direction === "horz") {
+            ctx.fillRect(this.x + 15, (this.y - 85), (this.length * l), 90)
+            ctx.stroke();
+        } else {
+            alert("Error in piece definition")
+        }        
     }
-
-    class Piece {
-        constructor(x, y, length, direction) {
-            this.length = length;
-            // Length can only be 2 or 3
-            this.x = x;
-            this.y = y;
-            this.direction = direction;
+    this.addEventListener("click", (e) => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        for (let i = 0; i < 1; i++) {
+            elements[i].drawPiece();        
         }
+        console.log(e)
+        var ferrari = new Piece(Math.floor(e.pageX / 100), 3, 2, "horz")
+        console.log(ferrari.x, ferrari.y)
+        ferrari.drawPiece();
+    })
+}
+// Instanciating the pieces
+var h1 = new Piece(3, 0, 3, "vert");
+h1.drawPiece();
+elements.push(h1)
+var ferrari = new Piece(3, 3, 2, "horz");
+ferrari.drawPiece();
+elements.push(ferrari);
+addEventListener
 
-        position() {
-            ctx.fillStyle = "tomato"
-            if (this.direction === "vert") {
-                ctx.fillRect((this.x * 100) + 5, (this.y * 100) + 5, 95, (this.length * 95));
-                ctx.stroke();
-            } else if (this.direction === "horz") {
-                ctx.fillRect((this.x * 100) + 15, ((this.y - 1) * 100) + 15, (this.length * 95), 90)
-                ctx.stroke();
-            } else {
-                alert("Error in piece definition")
-            }
-        }
-        fillVehicle() {
 
-        }
+var gameGrid = new Grid(6, 10);
+gameGrid.gridDrawing();
 
-    }
-    var ferrari = new Piece(0, 3, 2, "horz");
-    ferrari.position();
-    var gameGrid = new Grid(6, 10);
-    gameGrid.gridDrawing();
 
-}); //DOM Content closer
 
 // --------+== PseudoCode ==+-----------
 
