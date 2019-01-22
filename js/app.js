45eb5195a077b138c2d5b2f7e579ed3fddddc370
Michael Wright <mwright@spartaglobal.com>
2019-01-22 18:30:08 +0000

    Added win condition

--- js/app.js
+++ js/app.js
@@ -41,21 +41,21 @@ class Piece {
         this.com = []; 
         this.area = {
             tl: {
-                x: this.x + this.pd,
-                y: this.y + this.pd
+                x: this.x,
+                y: this.y
             },
             tr: {
-                x: this.x + (100 * this.w) + this.pd,
-                y: this.y + this.pd
+                x: this.x + (100 * this.w),
+                y: this.y
             },
             bl: {
-                x: this.x + this.pd,
-                y: this.y + (100 * this.h) +this.pd
+                x: this.x,
+                y: this.y + (100 * this.h)
             },
             br: {
-                x: this.x + (100 * this.w) + this.pd,
-                y: this.y + (100 * this.h) + this.pd
-            }
+                x: this.x + (100 * this.w),
+                y: this.y + (100 * this.h) 
+            } 
         }
         this.midpoints = {
             mt: {
@@ -96,19 +96,11 @@ class Piece {
             ctx.fillRect(this.x + 5, (this.y + 5), (this.length * l), 90);
             this.pieceArea = [[this.x, this.y], [this.x  + (this.length * 100), this.y], [this.x, this.y + 100], [this.x + (this.length * 100), this.y + 100]];
             ctx.stroke();
-            // for (let i = 0; i < this.length; i++) {
-            //     this.com.push([this.x + 50 + (i * 100), this.y - 50]);                
-            // }
         }        
     }
     
 }
 
-// Piece.prototype.block2 = {
-//     h: this.x + 50,
-//     v: this.y + 50
-// }
-// Instanciating the grid
 var gameGrid = new Grid(6, 0);
 gameGrid.gridDrawing();
 
@@ -116,40 +108,36 @@ gameGrid.gridDrawing();
 let ferrari;
 let v1;
 let v2;
+let v3;
+let v4;
 let h1;
 function init() {  
     elements = [];
+    // Winning piece
     ferrari = new Piece(1, 3, 2, "horz", 1, 2, 1);
     ferrari.drawPiece();
-    // ferrari.COM();
     elements.push(ferrari);
-    v2 = new Piece(4, 4, 2, "vert", 0, 1, 2);
-    v2.drawPiece();
-    // v2.COM();
-    elements.push(v2);
-    v1 = new Piece(3, 4, 3, "vert", 0, 1, 3);
+    // Verticle pieces
+    v1 = new Piece(4, 1, 3, "vert", 0, 1, 3);
     v1.drawPiece();
-    // v1.COM();
     elements.push(v1);
-    h1 = new Piece(3, 3, 3, "horz", 0, 3, 1)
+    v2 = new Piece(5, 4, 2, "vert", 0, 1, 2);
+    v2.drawPiece();
+    elements.push(v2);
+    v3 = new Piece(2, 5, 2, "vert", 0, 1, 2);
+    v3.drawPiece();
+    elements.push(v3);
+    v4 = new Piece(6, 4, 3, "vert", 0, 1, 3);
+    v4.drawPiece();
+    elements.push(v4);
+    // Horizontal pieces
+    h1 = new Piece(3, 5, 2, "horz", 0, 2, 1)
     h1.drawPiece();
-    // h1.COM();
     elements.push(h1);
 };
 init();
 
 // -----+== Collision ==+----
-// function collision() {
-//     for (let i = 0; i < elements.length; i++) {
-//         takenSpace.push(elements[i].com);
-//     };
-//     for (let j = 0; j < takenSpace.length; j++) {
-        
-//     }
-// }
-// collision();
-// console.log(takenSpace)
-
 
 
 // ------+== Reset Button ==+-------
@@ -160,10 +148,14 @@ reset.addEventListener('click', function() {
 })
 
 // -----+== Animation Loop ==+------
-function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ferrari.drawPiece();
+
+// Win Condition
+function checkWin() {
+    setTimeout(function() {
+        if (ferrari.area.tl.x == 400 && ferrari.area.tl.y == 200) {
+            alert("Win the ting");
+        }
+    }, 200)
 }
 
 // ------+== On click listeners ==+----
@@ -178,18 +170,26 @@ canvas.addEventListener("mousedown", (e) => {
 canvas.addEventListener("mouseup", (e) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     gameGrid.gridDrawing();
-    console.clear();
     for (let i = 0; i < elements.length; i++) {
         if (elements[i].direction == "horz" && elements[i].active == true) {
             elements[i].x = (Math.floor(e.pageX / 100) * 100);
+            elements[i].area.tl.x = elements[i].x;
+            elements[i].area.tl.y = elements[i].y;
+            elements[i].area.tr.x = elements[i].x + (elements[i].w * 100);
+            elements[i].area.tr.y = elements[i].y;
+            elements[i].area.bl.x = elements[i].x;
+            elements[i].area.bl.y = elements[i].y + (elements[i].h * 100);
+            elements[i].area.br.x = elements[i].x + (elements[i].w * 100);
+            elements[i].area.br.y = elements[i].y + (elements[i].h * 100);
             elements[i].active = false;
         } else if (elements[i].direction == "vert" && elements[i].active == true) {
             elements[i].y = (Math.floor(e.pageY / 100) * 100);
             elements[i].active = false;
         }
         elements[i].drawPiece();   
-        console.log(elements[i].area)
+        console.log(elements[i].area.tl)
     }
+    checkWin();
 });
 // --------+== PseudoCode ==+-----------
 
@@ -213,15 +213,3 @@ canvas.addEventListener("mouseup", (e) => {
 // Reset Button
 // Undo
 // Level Selector
-
-const player = {}
-
-player.block1 = {
-    x: 50,
-    y: 150
-}
-
-player.block2 = {
-    x: 150,
-    y: 150
-}
