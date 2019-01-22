9712d1a083b431663942160af925ff3ce8812b0e
Michael Wright <mwright@spartaglobal.com>
2019-01-22 17:10:34 +0000

    restructured the coordinate system

--- js/app.js
+++ js/app.js
@@ -4,9 +4,7 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 ctx.canvas.width = "610";
 ctx.canvas.height = "610";
-var elements = [],
-    tempElements = [];
-var pieceAreaArr = [];
+var elements = []
 
 class Grid {
     constructor(gridDim, pd) {
@@ -28,45 +26,90 @@ class Grid {
 }
 
 class Piece {
-    constructor(x, y, length, direction, imp) {
+    constructor(x, y, length, direction, imp, w, h) {
         this.length = length;
+        this.w = w;
+        this.h = h;
+        this.pd = 0;
         // Length can only be 2 or 3
-        this.x = x * 100;
-        this.y = y * 100;
+        this.x = (x - 1) * 100;
+        this.y = (y - 1) * 100;
         this.direction = direction;
         this.active = false;
         this.pieceArea = [[]];
         this.imp = imp;
+        this.com = []; 
+        this.area = {
+            tl: {
+                x: this.x + this.pd,
+                y: this.y + this.pd
+            },
+            tr: {
+                x: this.x + (100 * this.w) + this.pd,
+                y: this.y + this.pd
+            },
+            bl: {
+                x: this.x + this.pd,
+                y: this.y + (100 * this.h) +this.pd
+            },
+            br: {
+                x: this.x + (100 * this.w) + this.pd,
+                y: this.y + (100 * this.h) + this.pd
+            }
+        }
+        this.midpoints = {
+            mt: {
+                x: this.area.tl.x + 100,
+                y: this.area.tl.y
+            },
+            mr: {
+                x: this.area.tr.x,
+                y: this.area.tr.y + 100
+            },
+            mb: {
+                x: this.area.bl.x + 100,
+                y: this.area.bl.y
+            },
+            ml: {
+                x: this.area.tl.x,
+                y: this.area.tl.y + 100
+            }
+        }
+
     }
-    
     drawPiece() {
         var l = (((this.length * 100) - 10) / this.length);
+        this.com = [];
         if (this.direction === "vert") {
             ctx.fillStyle = "blue";
-            ctx.fillRect(this.x - 85, this.y + 15, 90, (this.length * l));
-            this.pieceArea = [[this.x - 100, this.y], [this.x, this.y], [this.x - 100, this.y + (this.length * 100)], [this.x, this.y + (this.length * 100)]];
+            ctx.fillRect(this.x + 5, this.y + 5, 90, (this.length * l));
+            this.pieceArea = [[this.x, this.y], [this.x + 100, this.y], [this.x, this.y + (this.length * 100)], [this.x + 100, this.y + (this.length * 100)]];
             ctx.stroke();
         } else if (this.direction === "horz" && this.imp == false) {
             ctx.fillStyle = "blue";
-            ctx.fillRect(this.x + 15, (this.y - 85), (this.length * l), 90)
-            this.pieceArea = [[this.x, this.y - 100], [this.x  + (this.length * 100), this.y - 100], [this.x, this.y], [this.x + (this.length * 100), this.y]];
+            ctx.fillRect(this.x + 5, (this.y + 5), (this.length * l), 90)
+            this.pieceArea = [[this.x, this.y], [this.x  + (this.length * 100), this.y], [this.x, this.y + 100], [this.x + (this.length * 100), this.y + 100]];
             ctx.stroke();
+            
         } else if (this.direction === "horz" && this.imp == true) {
             ctx.fillStyle = "tomato";
-            ctx.fillRect(this.x + 15, (this.y - 85), (this.length * l), 90);
-            this.pieceArea = [[this.x, this.y - 100], [this.x  + (this.length * 100), this.y - 100], [this.x, this.y], [this.x + (this.length * 100), this.y]];
+            ctx.fillRect(this.x + 5, (this.y + 5), (this.length * l), 90);
+            this.pieceArea = [[this.x, this.y], [this.x  + (this.length * 100), this.y], [this.x, this.y + 100], [this.x + (this.length * 100), this.y + 100]];
             ctx.stroke();
+            // for (let i = 0; i < this.length; i++) {
+            //     this.com.push([this.x + 50 + (i * 100), this.y - 50]);                
+            // }
         }        
-    }
-    reset() {
-        var l = (((this.length * 100) - 10) / this.length);
-
     }
     
 }
 
+// Piece.prototype.block2 = {
+//     h: this.x + 50,
+//     v: this.y + 50
+// }
 // Instanciating the grid
-var gameGrid = new Grid(6, 10);
+var gameGrid = new Grid(6, 0);
 gameGrid.gridDrawing();
 
 // Instanciating the pieces
@@ -76,29 +119,37 @@ let v2;
 let h1;
 function init() {  
     elements = [];
-    ferrari = new Piece(0, 3, 2, "horz", 1);
+    ferrari = new Piece(1, 3, 2, "horz", 1, 2, 1);
     ferrari.drawPiece();
+    // ferrari.COM();
     elements.push(ferrari);
-    v2 = new Piece(4, 2, 2, "vert", 0);
+    v2 = new Piece(4, 4, 2, "vert", 0, 1, 2);
     v2.drawPiece();
+    // v2.COM();
     elements.push(v2);
-    v1 = new Piece(3, 0, 3, "vert", 0);
+    v1 = new Piece(3, 4, 3, "vert", 0, 1, 3);
     v1.drawPiece();
+    // v1.COM();
     elements.push(v1);
-    h1 = new Piece(0, 4, 3, "horz", 0)
+    h1 = new Piece(3, 3, 3, "horz", 0, 3, 1)
     h1.drawPiece();
+    // h1.COM();
     elements.push(h1);
 };
 init();
-// let v2 = new Piece(4, 2, 2, "vert");
-// v2.drawPiece();
-// elements.push(v2);
-// let v1 = new Piece(3, 0, 3, "vert");
-// v1.drawPiece();
-// elements.push(v1)
-// let ferrari = new Piece(0, 3, 2, "horz");
-// ferrari.drawPiece();
-// elements.push(ferrari);
+
+// -----+== Collision ==+----
+// function collision() {
+//     for (let i = 0; i < elements.length; i++) {
+//         takenSpace.push(elements[i].com);
+//     };
+//     for (let j = 0; j < takenSpace.length; j++) {
+        
+//     }
+// }
+// collision();
+// console.log(takenSpace)
+
 
 
 // ------+== Reset Button ==+-------
@@ -121,13 +172,13 @@ canvas.addEventListener("mousedown", (e) => {
     for (let i = 0; i < elements.length; i++) {
         if (e.pageX > elements[i].pieceArea[0][0] && e.pageX < elements[i].pieceArea[1][0] && e.pageY > elements[i].pieceArea[0][1] && e.pageY < elements[i].pieceArea[2][1]){
             elements[i].active = true;
-            console.log(elements[i].active);
         }   
     }   
 });
 canvas.addEventListener("mouseup", (e) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     gameGrid.gridDrawing();
+    console.clear();
     for (let i = 0; i < elements.length; i++) {
         if (elements[i].direction == "horz" && elements[i].active == true) {
             elements[i].x = (Math.floor(e.pageX / 100) * 100);
@@ -136,24 +187,10 @@ canvas.addEventListener("mouseup", (e) => {
             elements[i].y = (Math.floor(e.pageY / 100) * 100);
             elements[i].active = false;
         }
-            elements[i].drawPiece();
-            console.log(elements[i].active)
-        }
+        elements[i].drawPiece();   
+        console.log(elements[i].area)
+    }
 });
-    
-
-
-
-    // console.log(e)
-    // var newferrari = new Piece(Math.floor(e.pageX / 100), 3, 2, "horz")
-    // newferrari.drawPiece();
-    // tempElements.push(newferrari)
-console.log(elements)
-
-
-
-
-
 // --------+== PseudoCode ==+-----------
 
 // ------ GRID -------
@@ -176,3 +213,15 @@ console.log(elements)
 // Reset Button
 // Undo
 // Level Selector
+
+const player = {}
+
+player.block1 = {
+    x: 50,
+    y: 150
+}
+
+player.block2 = {
+    x: 150,
+    y: 150
+}
